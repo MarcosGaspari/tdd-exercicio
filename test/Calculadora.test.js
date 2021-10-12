7ec2a84c51f8f558db67c42c1764aa4c2a776dad
marcos gaspari <marcosgaspari098@hotmail.com>
2021-10-12 00:49:11 -0300

    Adicionado teste para cargo DBA

--- test/Calculadora.test.js
+++ test/Calculadora.test.js
@@ -1,9 +1,13 @@
 const { expect } = require('@jest/globals');
 const Calculadora = require('../lib/calculadora');
 
-describe('Calculo DESENVOLVEDOR', () =>{
+describe('Calcula Salario', () =>{
     test('Teste desenvolvedor', () => {
         const res = Calculadora("Marcos Henrique", "marcos@hotmail.com",2.500,"Desenvolvedor");
         expect(res).toBe(2.250);
+    });
+    test('Teste DBA', () => {
+        const res = Calculadora("Carlos Henrique", "carlos@hotmail.com",2.200,"DBA");
+        expect(res).toBe(1.650);
     });
 });
